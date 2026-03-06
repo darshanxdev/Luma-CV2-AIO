18af82b4d6764cc71dd0134349c4e2bb95d4b8f7
Shreya Singh <shreyasingh722006@gmail.com>
2026-03-06 21:20:44 +0530

    fix: forbidden error in auto.py on_guild_join

--- cogs/events/auto.py
+++ cogs/events/auto.py
@@ -21,36 +21,40 @@ class Autorole (Cog ):
         data =await getConfig (guild .id )
         prefix =data .get ("prefix","!")
 
-        async for entry in guild .audit_logs (limit =3 ):
-            if entry .action ==discord .AuditLogAction .bot_add :
-                view = ui.LayoutView()
-                container = ui.Container(accent_color=None)
+        try:
+            async for entry in guild .audit_logs (limit =3 ):
+                if entry .action ==discord .AuditLogAction .bot_add :
+                    view = ui.LayoutView()
+                    container = ui.Container(accent_color=None)
 
-                container.add_item(ui.TextDisplay(f"<:icons_saturn:1372375229753593967> **Thanks for adding {BotName}!**"))
-                container.add_item(ui.Separator())
+                    container.add_item(ui.TextDisplay(f"<:icons_saturn:1372375229753593967> **Thanks for adding {BotName}!**"))
+                    container.add_item(ui.Separator())
 
-                content_text = (
-                    f"<a:GC_z_icon_rightarrow:1374279174751125534> My default prefix is `{prefix}`\n"
-                    f"<a:GC_z_icon_rightarrow:1374279174751125534> Use `{prefix}help` to see a list of commands\n"
-                    f"<a:GC_z_icon_rightarrow:1374279174751125534> For detailed guides, FAQ, and information, check out our help command!"
-                )
-                
-                bot_avatar_url = self.bot.user.avatar.url if self.bot.user.avatar else self.bot.user.default_avatar.url
-                
-                container.add_item(
-                    ui.Section(
-                        ui.TextDisplay(content_text),
-                        accessory=ui.Thumbnail(bot_avatar_url)
+                    content_text = (
+                        f"<a:GC_z_icon_rightarrow:1374279174751125534> My default prefix is `{prefix}`\n"
+                        f"<a:GC_z_icon_rightarrow:1374279174751125534> Use `{prefix}help` to see a list of commands\n"
+                        f"<a:GC_z_icon_rightarrow:1374279174751125534> For detailed guides, FAQ, and information, check out our help command!"
                     )
-                )
+                    
+                    bot_avatar_url = self.bot.user.avatar.url if self.bot.user.avatar else self.bot.user.default_avatar.url
+                    
+                    container.add_item(
+                        ui.Section(
+                            ui.TextDisplay(content_text),
+                            accessory=ui.Thumbnail(bot_avatar_url)
+                        )
+                    )
 
-                view.add_item(container)
+                    view.add_item(container)
 
-                try :
-                    await entry .user .send (view=view)
-                except Exception as e :
-                    print (f"Failed to send welcome message: {e}") 
-
+                    try :
+                        await entry .user .send (view=view)
+                    except Exception as e :
+                        print (f"Failed to send welcome message: {e}")
+        except discord.Forbidden:
+            print(f"Missing permissions to view audit logs in {guild.name}")
+        except Exception as e:
+            print(f"Error in on_guild_join: {e}")
 """
 : ! Aegis !
     + Discord: itsfizys
